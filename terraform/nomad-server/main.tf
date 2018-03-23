54f62680940fbd3d269d741c0652804204ab491f
stack72 <public@paulstack.co.uk>
2018-03-23 18:38:47 +0200

    backend/manta: Adding the remote state backend to nomad-server

--- terraform/nomad-server/main.tf
+++ terraform/nomad-server/main.tf
@@ -1,8 +1,15 @@
 provider "triton" {}
 
-resource "triton_machine" "nomad_server_blue" {
+terraform {
+  required_version = ">= 0.11.0"
+  backend "manta" {
+    path = "tsg-nomad-server"
+  }
+}
+
+resource "triton_machine" "nomad_server" {
   count            = "${var.nomad_server_count}"
-  name             = "${format("nomad%02d", count.index + 1)}"
+  name             = "${format("tsg-nomad-server-%02d", count.index + 1)}"
   package          = "${var.nomad_instance_package}"
   image            = "${data.triton_image.nomad_server.id}"
 
@@ -10,8 +17,14 @@ resource "triton_machine" "nomad_server_blue" {
     services = ["${var.nomad_cns_tag}"]
   }
 
+  tags {
+    name = "${format("tsg-nomad-server-%02d", count.index + 1)}"
+  }
+
   affinity    = ["instance!=nomad*"]
-  user_script = "${data.template_file.user_data.rendered}"
+  user_script = "${element(
+                   data.template_file.user_data.*.rendered,
+                   count.index)}"
   networks    = ["${data.triton_network.private.id}",
                  "${data.triton_network.public.id}"]
 }
@@ -35,8 +48,10 @@ data "triton_account" "main" {}
 data "triton_datacenter" "current" {}
 
 data "template_file" "user_data" {
+  count = "${var.nomad_server_count}"
   template = "${file("instance-user.conf")}"
   vars {
+    hostname = "${format("tsg-nomad-server-%02d", count.index + 1)}"
     dc = "${data.triton_datacenter.current.name}"
     cns_url = "${format("%s.svc.%s.%s.cns.joyent.com", var.consul_cns_tag, data.triton_account.main.id, data.triton_datacenter.current.name)}"
   }
