03ab96324aa9f9eb8d5c4d16ecf72b6c84f2e7e4
Krzysztof Wilczynski <kwilczynski@joyent.com>
2018-03-27 03:38:27 +0200

    Update template for the Bastion instance.
    Signed-off-by: Krzysztof Wilczynski <kwilczynski@joyent.com>

--- terraform/bastion/variables.tf
+++ terraform/bastion/variables.tf
@@ -1,3 +1,19 @@
+variable "name" {
+  default = "tsg"
+}
+
+variable "image_name" {
+  default = "tsg-base"
+}
+
+variable "image_version" {
+  default = "0.1.0"
+}
+
+variable "package" {
+  default= "k4-general-kvm-3.75G"
+}
+
 variable "allowed_ips" {
   default = []
 }
